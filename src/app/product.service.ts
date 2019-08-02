c58a43ccae856569f4fd29b457d39515a51eb54a
Steve <stevexblazer@gmail.com>
2019-08-02 16:29:12 +0530

    rxjs slide 22 even numbers

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http'; //should come automatically otherwise add manually
 import { of } from 'rxjs';
+import {filter} from 'rxjs/operators'
+
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +22,12 @@ export class ProductService {
 
   //  return this._http.get('http://localhost:4200/assets/product.json')
 
-  of(1,2,3,4,5).subscribe(v1 => {
+  of(1,2,3,4,5)
+  .pipe(
+    filter((num) =>
+     { return  (num%2==0) })
+  )
+  .subscribe(v1 => {
     console.log(v1)
   })
   }
