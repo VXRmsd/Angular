e5379f69fc7b5ac58c0fba52580f478b02837bc7
Steve <stevexblazer@gmail.com>
2019-08-02 16:42:54 +0530

    rxjs slide 22 with pipe

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http'; //should come automatically otherwise add manually
-import { of } from 'rxjs';
-import {filter} from 'rxjs/operators'
-import {map} from 'rxjs/operators'
+import { of,pipe } from 'rxjs';
+import {filter} from 'rxjs/operators';
+import {map} from 'rxjs/operators';
 
 
 
@@ -24,14 +24,16 @@ export class ProductService {
 
   //  return this._http.get('http://localhost:4200/assets/product.json')
 
-  of(1,2,3,4,5)
-  .pipe(
-    filter((num) =>
+  const stream=of(1,2,3,4,5,6)
+  const pipe1=pipe(
+    filter((num:number) =>
      { return  (num%2==0) 
     }),
     map(num => num * num)
   )
-  .subscribe(v1 => {
+
+  const obs=pipe1(stream)
+  obs.subscribe(v1 => {
     console.log(v1)
   })
   }
